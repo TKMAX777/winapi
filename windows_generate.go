c4565d1e9650964baf22bc1e72824634d40ae87d
TKMAX777 <service@tk-max.com>
2023-04-18 23:03:46 +0900

    update

--- windows_generate.go
+++ windows_generate.go
@@ -40,14 +40,25 @@ func errnoErr(e syscall.Errno) error {
 var (
 	modGdi32    = windows.NewLazySystemDLL("Gdi32.dll")
 	modMmdevapi = windows.NewLazySystemDLL("Mmdevapi.dll")
+	modWtsapi32 = windows.NewLazySystemDLL("Wtsapi32.dll")
 	moduser32   = windows.NewLazySystemDLL("user32.dll")
 
 	procCreateDIBSection            = modGdi32.NewProc("CreateDIBSection")
 	procCreatePen                   = modGdi32.NewProc("CreatePen")
 	procCreateRectRgnIndirect       = modGdi32.NewProc("CreateRectRgnIndirect")
 	procCreateSolidBrush            = modGdi32.NewProc("CreateSolidBrush")
+	procExtFloodFill                = modGdi32.NewProc("ExtFloodFill")
 	procPolyDraw                    = modGdi32.NewProc("PolyDraw")
 	procActivateAudioInterfaceAsync = modMmdevapi.NewProc("ActivateAudioInterfaceAsync")
+	procWTSCloseServer              = modWtsapi32.NewProc("WTSCloseServer")
+	procWTSEnumerateSessionsExW     = modWtsapi32.NewProc("WTSEnumerateSessionsExW")
+	procWTSFreeMemoryExW            = modWtsapi32.NewProc("WTSFreeMemoryExW")
+	procWTSOpenServerExW            = modWtsapi32.NewProc("WTSOpenServerExW")
+	procWTSVirtualChannelClose      = modWtsapi32.NewProc("WTSVirtualChannelClose")
+	procWTSVirtualChannelOpen       = modWtsapi32.NewProc("WTSVirtualChannelOpen")
+	procWTSVirtualChannelOpenEx     = modWtsapi32.NewProc("WTSVirtualChannelOpenEx")
+	procWTSVirtualChannelRead       = modWtsapi32.NewProc("WTSVirtualChannelRead")
+	procWTSVirtualChannelWrite      = modWtsapi32.NewProc("WTSVirtualChannelWrite")
 	procClipCursor                  = moduser32.NewProc("ClipCursor")
 	procEnumDesktopWindows          = moduser32.NewProc("EnumDesktopWindows")
 	procFillRect                    = moduser32.NewProc("FillRect")
@@ -55,6 +66,8 @@ var (
 	procGetClassNameW               = moduser32.NewProc("GetClassNameW")
 	procGetWindowTextW              = moduser32.NewProc("GetWindowTextW")
 	procInvalidateRect              = moduser32.NewProc("InvalidateRect")
+	procMapVirtualKeyW              = moduser32.NewProc("MapVirtualKeyW")
+	procRegisterClassExW            = moduser32.NewProc("RegisterClassExW")
 	procSetLayeredWindowAttributes  = moduser32.NewProc("SetLayeredWindowAttributes")
 	procSetWindowRgn                = moduser32.NewProc("SetWindowRgn")
 	procSetWindowTextW              = moduser32.NewProc("SetWindowTextW")
@@ -62,13 +75,13 @@ var (
 	procUpdateLayeredWindow         = moduser32.NewProc("UpdateLayeredWindow")
 )
 
-func CreateDIBSection(hdc uintptr, pbmi uintptr, usage uint, ppvBits uintptr, hSection uintptr, offset uint32) (hBitMap uintptr) {
+func createDIBSection(hdc uintptr, pbmi uintptr, usage uint, ppvBits uintptr, hSection uintptr, offset uint32) (hBitMap uintptr) {
 	r0, _, _ := syscall.Syscall6(procCreateDIBSection.Addr(), 6, uintptr(hdc), uintptr(pbmi), uintptr(usage), uintptr(ppvBits), uintptr(hSection), uintptr(offset))
 	hBitMap = uintptr(r0)
 	return
 }
 
-func CreatePen(iStyle int, cWidth int, color uint32) (hpen uintptr) {
+func createPen(iStyle int, cWidth int, color uint32) (hpen uintptr) {
 	r0, _, _ := syscall.Syscall(procCreatePen.Addr(), 3, uintptr(iStyle), uintptr(cWidth), uintptr(color))
 	hpen = uintptr(r0)
 	return
@@ -80,21 +93,100 @@ func createRectRgnIndirect(rect uintptr) (rgn uintptr) {
 	return
 }
 
-func CreateSolidBrush(color uint32) (hbrush uintptr) {
+func createSolidBrush(color uint32) (hbrush uintptr) {
 	r0, _, _ := syscall.Syscall(procCreateSolidBrush.Addr(), 1, uintptr(color), 0, 0)
 	hbrush = uintptr(r0)
 	return
 }
 
-func PolyDraw(hdc uintptr, apt uintptr, aj uintptr, cpt int) (ok bool) {
-	r0, _, _ := syscall.Syscall6(procPolyDraw.Addr(), 4, uintptr(hdc), uintptr(apt), uintptr(aj), uintptr(cpt), 0, 0)
-	ok = r0 != 0
+func extFloodFill(hdc uintptr, x int, y int, color uint32, opType uint32) (err error) {
+	r1, _, e1 := syscall.Syscall6(procExtFloodFill.Addr(), 5, uintptr(hdc), uintptr(x), uintptr(y), uintptr(color), uintptr(opType), 0)
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func polyDraw(hdc uintptr, apt uintptr, aj uintptr, cpt int) (err error) {
+	r1, _, e1 := syscall.Syscall6(procPolyDraw.Addr(), 4, uintptr(hdc), uintptr(apt), uintptr(aj), uintptr(cpt), 0, 0)
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
 	return
 }
 
 func activateAudioInterfaceAsync(deviceInterfacePath *uint16, riid uintptr, activationParams uintptr, completionHandler uintptr, createAsync uintptr) (hresult int32) {
 	r0, _, _ := syscall.Syscall6(procActivateAudioInterfaceAsync.Addr(), 5, uintptr(unsafe.Pointer(deviceInterfacePath)), uintptr(riid), uintptr(activationParams), uintptr(completionHandler), uintptr(createAsync), 0)
 	hresult = int32(r0)
+	return
+}
+
+func wtsCloseServerExW(hServer uintptr) {
+	syscall.Syscall(procWTSCloseServer.Addr(), 1, uintptr(hServer), 0, 0)
+	return
+}
+
+func wtsEnumerateSessionsEx(hServer uintptr, pLevel *uint32, Filter uint32, ppSessionInfo uintptr, pCount *uint32) (err error) {
+	r1, _, e1 := syscall.Syscall6(procWTSEnumerateSessionsExW.Addr(), 5, uintptr(hServer), uintptr(unsafe.Pointer(pLevel)), uintptr(Filter), uintptr(ppSessionInfo), uintptr(unsafe.Pointer(pCount)), 0)
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func wtsFreeMemoryEx(wtsTypeClass uintptr, pMemory uintptr, NumberOfEntries uint32) (err error) {
+	r1, _, e1 := syscall.Syscall(procWTSFreeMemoryExW.Addr(), 3, uintptr(wtsTypeClass), uintptr(pMemory), uintptr(NumberOfEntries))
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func wtsOpenServerExW(pServerName *uint16) (handle uintptr) {
+	r0, _, _ := syscall.Syscall(procWTSOpenServerExW.Addr(), 1, uintptr(unsafe.Pointer(pServerName)), 0, 0)
+	handle = uintptr(r0)
+	return
+}
+
+func wtsVirtualChannelClose(hChannelHandle uintptr) (err error) {
+	r1, _, e1 := syscall.Syscall(procWTSVirtualChannelClose.Addr(), 1, uintptr(hChannelHandle), 0, 0)
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func wtsVirtualChannelOpen(hServer uintptr, SessionId uint32, pVirtualName *uint16) (handle uintptr, err error) {
+	r0, _, e1 := syscall.Syscall(procWTSVirtualChannelOpen.Addr(), 3, uintptr(hServer), uintptr(SessionId), uintptr(unsafe.Pointer(pVirtualName)))
+	handle = uintptr(r0)
+	if handle == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func wtsVirtualChannelOpenEx(SessionId uint32, pVirtualName *byte, flags uint32) (handle uintptr, err error) {
+	r0, _, e1 := syscall.Syscall(procWTSVirtualChannelOpenEx.Addr(), 3, uintptr(SessionId), uintptr(unsafe.Pointer(pVirtualName)), uintptr(flags))
+	handle = uintptr(r0)
+	if handle == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func wtsVirtualChannelRead(hChannelHandle uintptr, TimeOut uint32, Buffer uintptr, BufferSize uint32, pBytesRead *uint32) (err error) {
+	r1, _, e1 := syscall.Syscall6(procWTSVirtualChannelRead.Addr(), 5, uintptr(hChannelHandle), uintptr(TimeOut), uintptr(Buffer), uintptr(BufferSize), uintptr(unsafe.Pointer(pBytesRead)), 0)
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func wtsVirtualChannelWrite(hChannelHandle uintptr, Buffer uintptr, Length uint32, pBytesWritten *uint32) (err error) {
+	r1, _, e1 := syscall.Syscall6(procWTSVirtualChannelWrite.Addr(), 4, uintptr(hChannelHandle), uintptr(Buffer), uintptr(Length), uintptr(unsafe.Pointer(pBytesWritten)), 0, 0)
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
 	return
 }
 
@@ -107,15 +199,19 @@ func clipCursor(rect uintptr) (ok int, err error) {
 	return
 }
 
-func enumDesktopWindows(hDesktop uintptr, lpEnumFunc uintptr, lParam uintptr) (ok bool) {
-	r0, _, _ := syscall.Syscall(procEnumDesktopWindows.Addr(), 3, uintptr(hDesktop), uintptr(lpEnumFunc), uintptr(lParam))
-	ok = r0 != 0
+func enumDesktopWindows(hDesktop uintptr, lpEnumFunc uintptr, lParam uintptr) (err error) {
+	r1, _, e1 := syscall.Syscall(procEnumDesktopWindows.Addr(), 3, uintptr(hDesktop), uintptr(lpEnumFunc), uintptr(lParam))
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
 	return
 }
 
-func fillRect(hdc uintptr, rect uintptr, hbr uintptr) (ok bool) {
-	r0, _, _ := syscall.Syscall(procFillRect.Addr(), 3, uintptr(hdc), uintptr(rect), uintptr(hbr))
-	ok = r0 != 0
+func fillRect(hdc uintptr, rect uintptr, hbr uintptr) (err error) {
+	r1, _, e1 := syscall.Syscall(procFillRect.Addr(), 3, uintptr(hdc), uintptr(rect), uintptr(hbr))
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
 	return
 }
 
@@ -137,35 +233,58 @@ func getWindowText(hwnd uintptr, lpString uintptr, nMax int) (length int) {
 	return
 }
 
-func invalidateRect(hwnd uintptr, rect uintptr, bErase bool) (ok bool) {
+func invalidateRect(hwnd uintptr, rect uintptr, bErase bool) (err error) {
 	var _p0 uint32
 	if bErase {
 		_p0 = 1
 	}
-	r0, _, _ := syscall.Syscall(procInvalidateRect.Addr(), 3, uintptr(hwnd), uintptr(rect), uintptr(_p0))
-	ok = r0 != 0
+	r1, _, e1 := syscall.Syscall(procInvalidateRect.Addr(), 3, uintptr(hwnd), uintptr(rect), uintptr(_p0))
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
 	return
 }
 
-func setLayeredWindowAttributes(hwnd uintptr, color uint32, bAlpha byte, dwFlags uint32) (ok bool) {
-	r0, _, _ := syscall.Syscall6(procSetLayeredWindowAttributes.Addr(), 4, uintptr(hwnd), uintptr(color), uintptr(bAlpha), uintptr(dwFlags), 0, 0)
-	ok = r0 != 0
+func mapVirtualKey(uCode uint32, uMapType uint32) (code uint32) {
+	r0, _, _ := syscall.Syscall(procMapVirtualKeyW.Addr(), 2, uintptr(uCode), uintptr(uMapType), 0)
+	code = uint32(r0)
 	return
 }
 
-func setWindowRgn(hwnd uintptr, hRgn uintptr, bRedraw bool) (ok bool) {
+func registerClassEx(windowClass uintptr) (atom uint16, err error) {
+	r0, _, e1 := syscall.Syscall(procRegisterClassExW.Addr(), 1, uintptr(windowClass), 0, 0)
+	atom = uint16(r0)
+	if atom == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func setLayeredWindowAttributes(hwnd uintptr, color uint32, bAlpha byte, dwFlags uint32) (err error) {
+	r1, _, e1 := syscall.Syscall6(procSetLayeredWindowAttributes.Addr(), 4, uintptr(hwnd), uintptr(color), uintptr(bAlpha), uintptr(dwFlags), 0, 0)
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
+	return
+}
+
+func setWindowRgn(hwnd uintptr, hRgn uintptr, bRedraw bool) (err error) {
 	var _p0 uint32
 	if bRedraw {
 		_p0 = 1
 	}
-	r0, _, _ := syscall.Syscall(procSetWindowRgn.Addr(), 3, uintptr(hwnd), uintptr(hRgn), uintptr(_p0))
-	ok = r0 != 0
+	r1, _, e1 := syscall.Syscall(procSetWindowRgn.Addr(), 3, uintptr(hwnd), uintptr(hRgn), uintptr(_p0))
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
 	return
 }
 
-func setWindowText(hwnd uintptr, lpString *uint16) (ok bool) {
-	r0, _, _ := syscall.Syscall(procSetWindowTextW.Addr(), 2, uintptr(hwnd), uintptr(unsafe.Pointer(lpString)), 0)
-	ok = r0 != 0
+func setWindowText(hwnd uintptr, lpString *uint16) (err error) {
+	r1, _, e1 := syscall.Syscall(procSetWindowTextW.Addr(), 2, uintptr(hwnd), uintptr(unsafe.Pointer(lpString)), 0)
+	if r1 == 0 {
+		err = errnoErr(e1)
+	}
 	return
 }
 
